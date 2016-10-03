43f17a2372d808537c155f72dcc7d501e0dc74a0
Parsa Amini <me@parsaamini.net>
2016-10-03 11:11:29 -0500

    fixing printers #6

--- tools/hpx-gdb/python/hpx/__init__.py
+++ tools/hpx-gdb/python/hpx/__init__.py
@@ -9,6 +9,10 @@
 # Distributed under the Boost Software License, Version 1.0. (See accompanying
 # file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 #
+
+commands = []
+pretty_printers = {}
+
 import gdb
 import sys
 import helpers.commands
@@ -18,9 +22,6 @@ from hpx.printers import *
 GDB_CMD_TYPE = gdb.COMMAND_NONE if 'COMMAND_USER' in dir(
     gdb
 ) else gdb.COMMAND_USER
-
-commands = []
-pretty_printers = {}
 
 
 def build_pretty_printers():
